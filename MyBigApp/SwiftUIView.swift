80682bd43db9d0303cba2b019d9ed2c6ff2f10cb
MIchel Khouri <mkhourisanchez@lcs.on.ca>
2026-03-01 13:50:19 -0500

    finished the subview

---
+++ MyBigApp/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  MyBigApp
+//
+//  Created by Michel Khouri on 2026-03-01.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
