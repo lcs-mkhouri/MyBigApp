93b2c3f2202ad61e44bf372703f7e04e9cff39e5
MIchel Khouri <mkhourisanchez@lcs.on.ca>
2026-03-01 13:10:06 -0500

    Initial Commit

---
+++ MyBigApp/MyBigAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBigAppApp.swift
+//  MyBigApp
+//
+//  Created by Michel Khouri on 2026-03-01.
+//
+
+import SwiftUI
+
+@main
+struct MyBigAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
