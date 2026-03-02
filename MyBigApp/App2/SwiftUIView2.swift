743ed720a2c229b5029bd3e72a613bd04f8f58e3
MIchel Khouri <mkhourisanchez@lcs.on.ca>
2026-03-02 11:16:26 -0500

    I reorganise the files putting them in folders in preparation of the second slide

---
+++ MyBigApp/App2/SwiftUIView2.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView2.swift
+//  MyBigApp
+//
+//  Created by Michel Khouri on 2026-03-02.
+//
+
+import SwiftUI
+
+struct SwiftUIView2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView2()
+}
